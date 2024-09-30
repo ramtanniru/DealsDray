d8743c59ed2f550ea3746d27356548dd61ef5151
ram_tanniru <ramtanniru2@gmail.com>
2024-09-30 18:41:29 +0530

    added routegaurd for home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,17 @@
+"use client"
+import { AuthContext } from "@/context/AuthContext";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect } from "react";
 
 export default function Home() {
+  const {isAuthenticated} = useContext(AuthContext);
+  const router = useRouter();
+  useEffect(()=>{
+    if(!isAuthenticated){
+      router.push('/login');
+    }
+  },[router,isAuthenticated]);
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-white">
       <div className="flex flex-col gap-5 justify-center items-center w-1/2">
